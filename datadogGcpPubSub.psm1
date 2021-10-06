d698e28a5d1913af6e890d91d04c2ec7a21403dc
Arthur Lago Oliveira <aloliveira@taginfraestrutura.com.br>
2021-10-05 23:15:07 -0300

    hotfix

--- datadogGcpPubSub.psm1
+++ datadogGcpPubSub.psm1
@@ -3,16 +3,18 @@ Using module .\datadogClass.psm1
 ## Funcoes para extrair dados de um topico do GCP PubSub
 class datadogGcpPubSub : datadog 
 {
+    [string]$projectId
     [string]$subscriptionId
 
     # Constructor
-    datadogGcpPubSub ([string]$APIKey, [string]$APPKey, [string]$subscriptionId) 
+    datadogGcpPubSub ([string]$APIKey, [string]$APPKey, [string]$projectId,[string]$subscriptionId) 
     {
         $this.APIKey = $APIKey
         $this.APPKey = $APPKey
         $this.headers.Add("Content-Type", "application/json")
         $this.headers.Add("DD-API-KEY", $this.APIKey)
         $this.headers.Add("DD-APPLICATION-KEY", $this.APPKey)
+        $this.projectId = $projectId
         $this.subscriptionId = $subscriptionId
     }
     
@@ -22,7 +24,7 @@ class datadogGcpPubSub : datadog
         $intervalSeconds = $interval * 60
         $epochTimestampNow = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
         $epochTimestampBefore = $epochTimestampNow - $intervalSeconds
-        $query = "max:gcp.pubsub.subscription.num_undelivered_messages{subscription_id:$($this.subscriptionId)}"    
+        $query = "max:gcp.pubsub.subscription.num_undelivered_messages{subscription_id:$($this.subscriptionId),project_id:$($this.projectId)}" 
         $queryEncoded = [uri]::EscapeDataString($query)
         $uri = "https://api.datadoghq.com/api/v1/query?from=$($epochTimestampBefore)&to=$($epochTimestampNow)&query=$($queryEncoded)"
         
