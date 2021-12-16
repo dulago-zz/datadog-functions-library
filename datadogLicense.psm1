c371a61a6f3b284a95021d5b24a57613e877d589
Arthur Lago Oliveira <aloliveira@taginfraestrutura.com.br>
2021-12-16 19:06:48 -0300

    adding license class

---
+++ datadogLicense.psm1
@@ -0,0 +1,16 @@
+Using module .\datadogClass.psm1
+using module .\datadogApm.psm1
+
+class datadogLicense : datadog 
+{
+    datadogApm([string]$APIKey, [string]$APPKey) 
+    {
+        $this.APIKey = $APIKey
+        $this.APPKey = $APPKey
+        $this.headers.Add("Content-Type", "application/json")
+        $this.headers.Add("DD-API-KEY", $this.APIKey)
+        $this.headers.Add("DD-APPLICATION-KEY", $this.APPKey)
+    }
+
+    
+}
